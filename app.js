426da868018ae6ad9f82dac7e9c5260ed8d9e8e3
XCA Student <xcastudent@Admins-MacBook-Pro.local>
2022-06-13 08:50:22 -0400

    xca/weatherapp/052022

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
+const { response } = require('express')
+
+const app = express()
+    // Set view engine to pug and establish view folder
+app.set('view engine', 'pug')
+app.set('views', './views')
+    // Middleware that processes incoming request
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/styles', express.static('styles'))
+app.use('/images', express.static('public'))
+
+// Get Route
+app.get('/', (req, res) => {
+        res.render('index')
+    })
+    // Post Route
+app.post("/location", (req, res) => {
+    let location = req.body.search
+    fetch(`http://api.weatherapi.com/v1/current.json?key=98985a28319148cca7f190051223105&q=${location}&aqi=no`)
+        .then(response => response.json())
+        .then((data) => {
+            const weather = {
+                "name": data.location.name,
+                "region": data.location.region,
+                "temp_f": data.current.temp_f,
+                "condition": data.current.condition.text,
+                "icon": data.current.condition.icon,
+                "humidity": data.current.humidity,
+            }
+            res.render('postPage', weather)
+        })
+
+})
+app.listen(3000, () => console.log("Server serving"))
